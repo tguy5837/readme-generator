2a4b6564d9a8da9534b1aefba77e54b1ee677f85
DTGuy <tguy5837@gmail.com>
2022-09-22 18:05:31 -0400

    connected index.js to generateMarkdown module

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your README? (Required)',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('Please enter a title for your README.');
-                return false;
-            };
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your README? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title for your README.');
+                    return false;
+                };
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please add a detailed description of the project here: ',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description for your README.');
+                    return false;
+                };
+            }
         }
-    }
-];
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
 
-// TODO: Create a function to initialize app
-function init(questions) {
-    return inquirer.prompt(questions);
-}
-
-// Function call to initialize app
-init(questions).then(questionResponses => {
-    console.log(questionResponses);;
-})
+// Function calls
+promptUser()
+    .then(readmeData => {
+        generateMarkdown(readmeData);
+    })
